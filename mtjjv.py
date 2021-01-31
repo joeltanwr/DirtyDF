e367b21aaf321e8d0cd156995e78e12ddd9eccfb
Vik <viknesh.gopal@gmail.com>
2021-01-31 23:41:16 +0800

    added table plot, example on partial function applciation

--- mtjjv.py
+++ mtjjv.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from functools import partial
 
 
 class DirtyDataFrame:
@@ -25,8 +27,9 @@ class DirtyDataFrame:
     def list_stainers(self):
         pass
 
-    def attach_stainer(self, f):
-        pass
+    def attach_stainer(self, f0, **kwargs):
+        f1 = partial(f0, **kwargs)
+        self.stainer_list.append(f1)
 
     def check_order(self):
         pass
@@ -41,19 +44,54 @@ class DirtyDataFrame:
         pass
 
 
-def add_duplicates(df, rows_to_dup=None, times_to_dup= None, dup_at=None,
-                   new_index=None):
+def add_duplicates(df, row_to_dup=None, times_to_dup= None, dup_at=None,
+                   new_index='keep'):
     """Stainer to duplicate rows of a dataset.
 
-    rows_to_dup:  integer-based indexes to df
+    row_to_dup:  integer-based indexes to df
     times_to_dup: array like or integer. If array, it should have the same 
                   length as rows_to_dup.
     dup_at:       Keep at original position, or shuffle.
     new_index:    The new index to use for the new data frame, or to keep
-                  duplicate values, or to use running integers.
+                  duplicate values, or to use running integers. This can be 
+                  'keep' or 'reindex'.
     """
 
-    if rows_to_dup is None:
-        pass
+    if row_to_dup is None:
+        row_to_add = df.sample(n=1)
+        row_to_dup = row_to_add.index[0]
+        #new_df = pd.concat([df.iloc[:index_to_add, :], row_to_add, 
+        #                    df.iloc[index_to_add:, :]])
+    else:
+        row_to_add = df.iloc[[row_to_dup]]
         
+    new_df = pd.concat([df.iloc[:row_to_dup, :], row_to_add, 
+                        df.iloc[row_to_dup:, :]])
 
+    if new_index is not None:
+        if new_index == 'keep':
+            return(new_df)
+        elif new_index == 'reindex':
+            new_df.index = np.arange(new_df.shape[0])
+        #else:
+        #    new_df.index = new_index
+
+    return(new_df)
+
+def df_plot(pandas_df):
+    # get shape
+    df_shape = pandas_df.shape
+
+    cell_colours = np.empty(df_shape, dtype=np.object)
+    cell_colours[:] = 'xkcd:lightblue'
+
+    #ax = plt.gca()
+    #plt.gca()
+    ax2 = plt.table(cellColours=cell_colours, loc='center')
+
+    #ax2 = ax.table(cellColours=cell_colors, loc='center', colWidths=[0.5, 0.5])
+
+    for cell in ax2.get_celld().values():
+        cell.set_height(1.0/df_shape[0])
+    ax2.axes.set_axis_off()
+    return(ax2)
