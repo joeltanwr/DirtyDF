63ede1ec811b9be5d2c432a936d12029e081d0d5
Vik <viknesh.gopal@gmail.com>
2021-07-05 23:46:40 +0800

    pip wheel uploaded

---
+++ ddf/README.md
@@ -0,0 +1,11 @@
+## Introduction
+
+This purpose was written to facilitate generation of "similar" datasets based
+on a single dataset. The original intention was to "dirty" a dataset before
+providing it to students for classroom assignments.
+
+There are three classes defined here: DirtyDF, Stainer, and History. 
+
+Stainers Dirty a DataFrame.
+
+Custom stainers can be written to create new stains.
--- ddf/setup.py
+++ ddf/setup.py
@@ -1,14 +1,31 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='ddf',
-    version='0.0.1',
+    name='dirtydf',
+    version='0.0.22',
     packages=['ddf'],
-    install_requires=[
-        'pandas',
-        'statsmodels',
-        'scipy',
-        'inflection',
-        'numpy; python_version >= "3.8"',
+    author="Joel Tan, Justin Lo, Vik Gopal",
+    author_email="vik.gopal@nus.edu.sg",
+    description="A package to dirty datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joeltanwr/DirtyDF", 
+    license="MIT",
+    platforms="OS Independent",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires ='>=3.6',
+    install_requires=['pandas>=1.2.1', 'statsmodels>=0.12.1', 
+    'scipy>=1.6.0', 'inflection>=0.5.1', 'numpy>=1.19.5']
+    #    'pandas',
+    #    'statsmodels >= 0.12.1',
+    #    'scipy',
+    #    'inflection',
+    #    'numpy; python_version >= "3.8"', ],
 )
